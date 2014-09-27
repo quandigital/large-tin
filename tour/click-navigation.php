4cf59b61f8958fa94aa102f10d316469631b5d10
alpipego <alpipego@gmail.com>
2014-09-27 14:28:38 +0200

    removed the tour

--- tour/click-navigation.php
+++
@@ -1,3 +0,0 @@
-<div id="clicknav" data-message="<?php __( 'Get on Board!', 'text on navigation after last island', 'quan' ); ?>">
-	<i class="ion-ios7-arrow-down" id="start-navigation"></i>
-</div>
