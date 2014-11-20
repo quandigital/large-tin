f999b00d4ed8431b1d84ef87637e39cfdca6ec62
alpipego <alpipego@gmail.com>
2014-11-20 10:11:41 +0100

    added cssunit functions

--- js/_functions.js
+++ js/_functions.js
@@ -6,3 +6,15 @@ function breakpoints() {
     
     return breakpoint;
 }
+
+$.fn.extend({
+    cssUnit: function(cssDeclaration) {
+        var cssDec = $(this).css(cssDeclaration);
+        return cssUnit(cssDec);
+    }
+});
+
+function cssUnit(cssValue) {
+    var cssArr = cssValue.match("([0-9]*)([a-z]*)");
+    return [cssArr[1], cssArr[2]];
+}
