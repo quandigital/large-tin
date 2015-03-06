b43326fbef248a62bfefc8a92bf5668747eabf36
alpipego <alpipego@gmail.com>
2015-03-06 10:46:17 +0100

    markup of jobs and job application sidebar done

--- single-quan_jobs.php
+++ single-quan_jobs.php
@@ -3,49 +3,20 @@
 	//job openings
 
 	get_header();
-
-	echo '<div class="content">';
-			if( have_posts() ) : 
-				while (have_posts()) : 
-					the_post(); 
-
-				the_title( '<h1>', '</h1>' );
-
-				echo '<article>';
-
-					if( isset( $_COOKIE['resolution'] ) ) {
-						$cookie_val = explode( ",", $_COOKIE['resolution'] );
-						$screen     = $cookie_val[0] * $cookie_val[1];
-						if( $screen <= 480 ) {
-							$thumb_size = 'small';
-							$dummy_size = '480x270';
-						} elseif( $screen <= 960 ) {
-							$thumb_size = 'medium';
-							$dummy_size = '960x540';
-						} elseif( $screen <= 1920 ) {
-							$thumb_size = 'large';
-							$dummy_size = '1920x1080';
-						} else {
-							$thumb_size = 'xlarge';
-							$dummy_size = '3840x2160';
-						}
-					} else {
-						$thumb_size = 'large';
-						$dummy_size = '1920x1080';
-					}
-
-					if( has_post_thumbnail() ) {
-						the_post_thumbnail( $thumb_size );
-					}
-					
-					the_content();
-				echo '</article>';
-
-				endwhile; 
-			endif; 
+	if( have_posts() ) : 
+		while (have_posts()) : 
+			the_post(); 
+?>
+	<div class="content">
+		<article>
+			<h1><?= get_the_title(); ?></h1>
+			<?php the_content(); ?>
+		</article>
+	</div>
+<?php
+		endwhile; 
+	endif; 
 		
-		get_sidebar( 'jobs' );
-
-	echo '</div>';
+	get_sidebar( 'jobs' );
 
 	get_footer();
