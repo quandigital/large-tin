1a6b762263775cd9a3a988e5a7d91662c33b77e1
alpipego <alpipego@gmail.com>
2015-03-06 10:49:48 +0100

    generic/general changes

--- comment-form.php
+++ comment-form.php
@@ -2,4 +2,6 @@
 	//custom comment-form
 ?>
 
-	
+    <div class="twitter">
+        <a href="https://twitter.com/intent/tweet?original_referer=<?php the_permalink(); ?>&source=tweetbutton&text=<?php echo $share_title; ?>&url=<?php the_permalink(); ?>&via=quandigital" data-popup="true"><i class="ion-social-twitter"></i> <span><?php _e( 'on twitter', 'quan' ); ?></span></a>
+    </div>	
