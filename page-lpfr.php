9b554e31a5663c70a5d9e6b41c3e93a3d96517d9
alpipego <alpipego@gmail.com>
2015-04-24 13:16:50 +0200

    template and styles for the french landing page

---
+++ page-lpfr.php
@@ -0,0 +1,71 @@
+<?php
+    
+    /**
+     * Template Name: French Landing Page
+     */
+    
+    get_header();
+?>
+    <section class="introduction">
+        <div class="contents">
+            <h1>Avez-vous besoin de plus de trafic ?</h1>
+            <h2>Notre équipe est déterminée à débloquer votre potentiel sur internet</h2>
+            <p>Nous aidons des marques françaises et internationales à développer leur visibilité et leurs revenus sur internet depuis près de 10 ans. Notre double expertise en SEO et Marketing nous permet de concevoir et exécuter des campagnes à fort ROI. Demandez dès maintenant une consultation gratuite avec l'un de nos spécialistes afin qu'il vous livre les clés pour optimiser votre trafic et vos revenus sur le web.</p>
+        </div>
+        <form id="contact" action="">
+            <input type="text" name="name" placeholder="Nom">
+            <input type="tel" name="phone" placeholder="Téléphone">
+            <input type="email" name="email" placeholder="Mail">
+            <input type="text" name="company" placeholder="Entreprise">
+            <input type="text" name="website" placeholder="Site Internet">
+            <div class="button submit">
+                Demander ma consultation gratuite
+            </div>
+        </form>    
+    </section>
+
+    <section class="services-1">
+        <div class="contents">
+            <h2>La performance est au coeur de nos valeurs</h2>
+            <div class="collab service">
+                <h3>Une collaboration durable</h3>
+                <p>Nous travaillons depuis plusieurs années avec la plupart nos clients. Développer le trafic d'un site internet de manière durable est un travail de longue haleine.</p>
+            </div>    
+            <div class="trafic service">
+                <h3>Plus de trafic</h3>
+                <p>Générer du trafic vers un site internet est notre spécialité. Nous vous aidons à développer une stratégie pour augmenter votre trafic de manière continue.</p>
+            </div>
+            <div class="revenus service">
+                <h3>Plus de revenus</h3>
+                <p>Le trafic n'a d'intérêt que s'il apporte des conversions. Nous nous concentrons sur génération de trafic qualifié afin de booster aussi vos revenus.</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="services-2">
+        <div class="contents">
+            <h2>Une approche intégrée du marketing en ligne</h2>
+            <div class="analyse service">
+                <h3>Analyser</h3>
+                <p>Comprendre le comportement de vos clients sur internet est primordial pour exécuter une campagne efficace. S'assurer que votre site répond aux meilleures pratiques d'usabilité et d'indexabilité est la base du succès.</p>
+            </div>    
+            <div class="contenu service">
+                <h3>Créer du contenu</h3>
+                <p>Le contenu d'un site est primordial au sein d'une stratégie de marketing en ligne pertinente et durable, que ce soit pour booster votre SEO ou vos conversions.</p>
+            </div>
+            <div class="marque service">
+                <h3>Promouvoir votre marque</h3>
+                <p>Nous vous aidons à communiquer auprès des influenceurs et les journalistes. Obtenir des backlinks et mentions sur des sites influents est une condition nécessaire pour booster votre SEO et votre image de marque.</p>
+            </div>
+        </div>
+    </section>
+
+    <section class="callout">
+        <div class="text">Commençons à booster votre trafic !</div>
+        <div class="cta">
+            <a href="#contact">Demander ma consultation gratuite</a>
+        </div>
+    </section>
+
+<?php
+    get_footer();
