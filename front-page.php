d58ad08ce29995b5ad6b32e069c6a1e62bc071f6
alpipego <alpipego@gmail.com>
2015-04-27 16:42:20 +0200

    front page hyphenation, call to action

--- front-page.php
+++ front-page.php
@@ -33,12 +33,15 @@
                 </section>
                 <section class="optimization">
                     <h3>ranking &amp; conversion optimization</h3>
-                    <p>Mit uns erzielen Sie einen Anstieg Ihrer Besucherzahlen sowie nachhaltig konvertierenden Traffic auf Ihrer Webseite. Unsere Maßnahmen sind an Ihren individuellen Zielen ausgerichtet und umfassen beispielsweise die Verbesserung Ihrer Webseiten-Struktur oder eine Layout- oder Warenkorb-Optimierung.</p>
+                    <p>Mit uns erzielen Sie einen Anstieg Ihrer Besucher&shy;zahlen sowie nachhaltig konvertierenden Traffic auf Ihrer Webseite. Unsere Maßnahmen sind an Ihren individuellen Zielen ausgerichtet und umfassen beispielsweise die Verbesserung der Struktur oder des Layouts Ihrer Webseite.</p>
                 </section>
                 <section class="analysis">
                     <h3>data acquisition &amp; evaluation</h3>
-                    <p>Detaillierte Konkurrenzanalysen sowie einmalige oder regelmäßige Onpage- und Offpage-Audits sind richtungsweisende Elemente unseres ganzheitlichen Beratungskonzepts. Wir schaffen auf Basis der erhobenen Daten eine individuell ausgerichtete Lösungsstrategie.</p>
+                    <p>Detaillierte Konkurrenz&shy;analysen sowie einmalige oder regelmäßige Onpage- und Offpage-Audits sind richtungs&shy;weisende Elemente unseres ganzheitlichen Beratungskonzepts. Wir schaffen auf Basis der erhobenen Daten eine individuell ausgerichtete Lösungsstrategie.</p>
                 </section>
+                <div class="cta">
+                    <div class="button">Work with us</div>
+                </div>
             </div>
         </section>
 
@@ -48,16 +51,19 @@
                 <p class="section-tagline">Unser Outreach-Marketing entfacht Begeisterung für Ihre Marke</p>
                 <section class="brand">
                     <h3>brand building</h3>
-                    <p>Wir kommunizieren die Werte Ihrer Marke an relevante Zielgruppen und empfehlen Ihnen strategisch wirksame Einsatzgebiete. Effiziente Online-Kommunikation in Verbindung mit zielgruppengerechten Inhalten verhelfen Ihnen zu einer langfristigen organischen Sichtbarkeit sowie zu einer erhöhten Markenbekanntheit.</p>
+                    <p>Wir kommunizieren die Werte Ihrer Marke an relevante Zielgruppen und empfehlen Ihnen strategisch wirksame Einsatzgebiete. Effiziente Online-Kommunikation in Verbindung mit ziel&shy;gruppengerechten Inhalten verhelfen Ihnen zu einer langfristigen organischen Sichtbarkeit sowie zu einer erhöhten Markenbekanntheit.</p>
                 </section>
                 <section class="content">
                     <h3>content marketing</h3>
-                    <p>Unser Content-Outreach basiert auf dem Empfehlungsmarketing und schafft die Nähe zu Ihrer Zielgruppe. Erreichen Sie die richtigen Menschen zur richtigen Zeit und lassen Sie sich beim Erzählen Ihrer Geschichte unter die Arme greifen - professionell und überzeugend.</p>
+                    <p>Unser Content-Outreach basiert auf dem Emp&shy;fehlungsmarketing und schafft die Nähe zu Ihrer Zielgruppe. Erreichen Sie die richtigen Menschen zur richtigen Zeit und lassen Sie sich beim Erzählen Ihrer Geschichte unter die Arme greifen &ndash; professionell und überzeugend.</p>
                 </section>
                 <section class="social">
                     <h3>social seeding</h3>
-                    <p>Wir entwickeln eine Social-Sharing Strategie für Sie, die Ihren Quality Content auf gut sichtbaren Internetplattformen wie Blogs und sozialen Netzwerken in der ganzen Welt verteilt. Profitieren Sie von unseren gut vernetzten Publishern und wecken Sie das Interesse an Ihrer Marke.</p>
+                    <p>Wir entwickeln eine Social-Sharing Strategie für Sie, die Ihren Quality Content auf gut sicht&shy;baren Internetplatt&shy;formen wie Blogs und sozialen Netzwerken in der ganzen Welt verteilt. Profitieren Sie von unseren gut vernetzten Publishern und wecken Sie das Interesse an Ihrer Marke.</p>
                 </section>
+                <div class="cta">
+                    <div class="button">Work with us</div>
+                </div>
             </div>
         </section>
 
@@ -67,16 +73,19 @@
                 <p class="section-tagline">Wir arbeiten mit höchstem Einsatz und kompromissloser Orientierung an Ihren Bedürfnissen</p>
                 <section class="commitment">
                     <h3>commitment</h3>
-                    <p>Mit großer Hingabe erfüllen wir die höchsten Ansprüche und legen dabei großen Wert auf transparente Kommunikation. Lernen Sie unsere qualifizierten Mitarbeiter kennen, gewinnen Sie Einblick in unsere Arbeitsweisen und behalten Sie jederzeit einen Überblick über die erbrachten Leistungen.</p>
+                    <p>Mit großer Hingabe erfüllen wir die höchsten Ansprüche und legen dabei großen Wert auf transparente Kommu&shy;nikation. Lernen Sie unsere qualifizierten Mitarbeiter kennen, gewinnen Sie Einblick in unsere Arbeitsweisen und behalten Sie jederzeit einen Überblick über die erbrachten Leistungen.</p>
                 </section>
                 <section class="direct">
                     <h3>immediate &amp; personal contact</h3>
-                    <p>Trotz der allgegenwärtigen digitalen Kommunikation begeistern wir uns weiterhin für das persönliche Gespräch. Mit einem persönlichen Ansprechpartner für den gesamten Kooperationszeitraum schenken wir Ihnen individuelle Aufmerksamkeit und stehen Ihnen gern und jederzeit zur Verfügung. </p>
+                    <p>Trotz der allgegenwär&shy;tigen digitalen Kommu&shy;nikation begeistern wir uns weiterhin für das persönliche Gespräch. Mit einem persönlichen Ansprechpartner für den gesamten Kooperations&shy;zeitraum schenken wir Ihnen individuelle Aufmerksamkeit und stehen Ihnen gern und jederzeit zur Verfügung. </p>
                 </section>
                 <section class="cost-benefit">
                     <h3>cost-benefit awareness</h3>
-                    <p>Unsere Leistungen werden stetig an die rasanten Entwicklungen im Onlinebereich angepasst. Damit garantieren wir Ihnen höchste Qualität und eine zeitgemäße Unterstützung bei Ihrer Webseiten-Optimierung zu einem sehr guten Preis-Leistungs-Verhältnis.</p>
+                    <p>Unsere Leistungen werden stetig an die rasanten Entwicklungen im Onlinebereich ange&shy;passt. Damit garantieren wir Ihnen höchste Quali&shy;tät und eine zeitgemäße Unterstützung bei Ihrer Webseiten-Optimierung zu einem sehr guten Preis-Leistungs-Verhältnis.</p>
                 </section>
+                <div class="cta">
+                    <div class="button">Work with us</div>
+                </div>
             </div>
         </section>
     </article>
