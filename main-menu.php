56d6b319ad06b386e7472944a5f89612c9176b81
alpipego <alpipego@gmail.com>
2015-04-28 15:00:29 +0200

    removed contact button on contact, fixed after phone element

--- main-menu.php
+++ main-menu.php
@@ -15,41 +15,6 @@
             <li><a href="<?= get_bloginfo('wpurl'); ?>/contact"><?php _e( 'Contact', 'quan' ); ?></a></li>
         </ul>
 
-        <?php
-            $lang = $GLOBALS['lang'];
-
-            if( $lang == 'de' ) {
-                $notlang = 'German';
-            } else if( $lang == 'fr' ) {
-                $notlang = 'French';
-            } else if( $lang == 'en' ) {
-                $notlang = 'English';
-            } else if( $lang == 'it' ) {
-                $notlang = 'Italian';
-            } else if( $lang == 'es' ) {
-                $notlang = 'Spanish';
-            }
-
-            function select_lang( $value, $lang ) {
-                if( $lang == $value ) {
-                    echo 'selected';
-                }
-            }
-        ?>
-
-            <div class="notlang">
-                <span><?php echo $notlang; ?> is not your language? Change it here: </span>
-                <div class="select">
-                    <select id="lang-change">
-                        <option value="en" <?php select_lang( 'en', $lang ); ?>>English</option>
-                        <option value="fr" <?php select_lang( 'fr', $lang ); ?>>French</option>
-                        <option value="de" <?php select_lang( 'de', $lang ); ?>>German</option>
-                        <option value="it" <?php select_lang( 'it', $lang ); ?>>Italian</option>
-                        <option value="es" <?php select_lang( 'es', $lang ); ?>>Spanish</option>
-                    </select>
-                </div>
-            </div>
-
         <div class="social-menu">
             <div class="mail">
                 <a href="mailto:info@quandigital.com"><?php _e( 'Write us an e-mail', 'quan' ); ?></a>
@@ -57,28 +22,6 @@
             <div class="phone">
                 <a href="<?php _ex( 'tel:+123456789', 'public phone number for your country', 'quan' ); ?>"><?php _e( 'Talk to us', 'quan' ); ?></a>
             </div>
-            <?php
-                /*
-                <div class="blog">
-                    <a href="<?php echo get_bloginfo( 'wpurl' ) . '/blog'; ?>"><?php _e( 'Read our blog', 'quan' ); ?></span></a>
-                </div>
-                <div class="facebook">
-                    <a href="https://www.facebook.com/quandigital" target="_blank">
-                        <span><?php _e( 'Like us on  facebook', 'quan' ); ?></span>
-                    </a>
-                </div>
-                <div class="twitter">
-                    <a href="https://twitter.com/quandigital" target="_blank">
-                        <span><?php _e( 'Follow us on twitter', 'quan' ); ?></span>
-                    </a>
-                </div>
-                <div class="gplus">
-                    <a href="https://plus.google.com/115674308506276261482/posts" target="_blank">
-                         <span><?php _e( 'Add us to your circles', 'quan' ); ?></span>
-                    </a>
-                </div>
-                */
-            ?>
             <div class="legal">
                 <a href="<?= get_bloginfo('wpurl'); ?>/impressum"><?php _e( 'Read the legal stuff', 'quan' ); ?></a>
             </div>
