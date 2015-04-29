8d7a3fc75fb8e7d039dd4480288991b99064ec20
alpipego <alpipego@gmail.com>
2015-04-29 16:33:16 +0200

    remove complicated language cookie stuff

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <?php 
-    require_once( __DIR__ . '/lang.php' );
+    // require_once( __DIR__ . '/lang.php' );
     require_once( __DIR__ . '/resolution.php' );
 
     // get the number of open jobs
