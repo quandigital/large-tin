c4563996f3e03c9e4069053f821a5389b52cdb64
alpipego <alpipego@gmail.com>
2015-09-09 10:15:46 +0200

    added hotjar script to header

--- header.php
+++ header.php
@@ -14,7 +14,22 @@
 	<meta charset="utf-8" />
     <meta name="viewport" content="user-scalable=no, width=device-width, initial-scale=1, maximum-scale=1">
 
-	<?php wp_head(); ?>
+	<?php 
+        wp_head();
+        if (!is_user_logged_in()) :
+    ?>
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:4286,hjsv:5};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
+    <?php endif; ?>
+
 </head>
 <body <?php body_class(); ?>>
 
